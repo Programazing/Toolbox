1a2cd304ea6f2ab82c33493bf98076d19bc934c4
Programazing <chris@thatamazingprogrammer.com>
2020-08-12 21:52:05 -0400

    Add option to Ignore Case within GetEnumFromName

--- DotNet/Core.Tests/EnumTests/GetEnumFromNameTests.cs
+++ DotNet/Core.Tests/EnumTests/GetEnumFromNameTests.cs
@@ -25,6 +25,22 @@ namespace Core.Tests.EnumTests
             sut.Should().Throw<ArgumentException>();
         }
 
+        [Test]
+        public void GetEnumFromName_WillThrowArgumentException_WhenGiven_AnExistingKey_WithIncorrectCasing()
+        {
+            Action sut = () => Enums.GetEnumFromName<Colors>("blue");
+
+            sut.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GetEnumFromName_WillReturnAnEnum_WhenGiven_AnExistingKey_WithIncorrectCasing()
+        {
+            var sut = Enums.GetEnumFromName<Colors>("blue", ignoreCase: true);
+
+            sut.Should().Be(Colors.Blue);
+        }
+
         private enum Colors
         {
             Blue = 1
--- DotNet/Core/Enums.cs
+++ DotNet/Core/Enums.cs
@@ -6,11 +6,11 @@ namespace Core
 {
     public static class Enums
     {
-        public static T GetEnumFromName<T>(string name)
+        public static T GetEnumFromName<T>(string name, bool ignoreCase = false)
         {
             try
             {
-                return (T)Enum.Parse(typeof(T), name);
+                return (T)Enum.Parse(typeof(T), name, ignoreCase);
             }
             catch (ArgumentException)
             {
