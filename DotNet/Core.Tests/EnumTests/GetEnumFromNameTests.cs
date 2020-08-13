105c296cc6a3a9c3e1b3c3142257cb96aedbf151
Programazing <chris@thatamazingprogrammer.com>
2020-08-12 21:07:28 -0400

    Add GetEnumFromNameTests

---
+++ DotNet/Core.Tests/EnumTests/GetEnumFromNameTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Tests.EnumTests
+{
+    [TestFixture]
+    public class GetEnumFromNameTests
+    {
+        [Test]
+        public void GetEnumFromName_WillReturnAnEnum_WhenGiven_AnExistingKey()
+        {
+            var sut = Enums.GetEnumFromName<Colors>("Blue");
+
+            sut.Should().Be(Colors.Blue);
+        }
+
+        private enum Colors
+        {
+            Blue = 1
+        }
+    }
+}
