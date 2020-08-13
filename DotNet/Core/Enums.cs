01d92668d4edc9bd0e25c569388759a6b0e81ce7
Programazing <chris@thatamazingprogrammer.com>
2020-08-12 21:07:04 -0400

    Add Enums Class

---
+++ DotNet/Core/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public static class Enums
+    {
+        public static T GetEnumFromName<T>(string name)
+        {
+            try
+            {
+                return (T)Enum.Parse(typeof(T), name);
+            }
+            catch (ArgumentException)
+            {
+                throw;
+            }
+        }
+    }
+}
